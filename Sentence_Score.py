6973bb5e0bfdc4a67db1ab8ff2c967f48c727323
kanujain <46599236+kanujain@users.noreply.github.com>
2020-07-12 01:25:10 +0530

    Update Sentence_Score.py

--- Sentence_Score.py
+++ Sentence_Score.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 
 import nltk
 import Sentence_Tokenization
+import Word_Frequency
 
 def calc_sentence_score:
     sentence_scores = {}
     sentence_tokens = Sentence_Tokenization.sentence_tokenization()
+    word_frequencies = Word_Frequency.calc_freq_word()
     for sentence in sentence_tokens:
         for word in nltk.word_tokenize(sentence.lower()):
             if word in word_frequencies.keys():
-                # if len(sentence.split(' ')) < 30:
-                if sentence not in sentence_scores:
-                    sentence_scores[sentence] = word_frequencies[word]
-                else:
-                    sentence_scores[sentence] += word_frequencies[word]
+                if len(sentence.split(' ')) < 30:
+                    if sentence not in sentence_scores:
+                        sentence_scores[sentence] = word_frequencies[word]
+                    else:
+                        sentence_scores[sentence] += word_frequencies[word]
     
     return sentence_scores
 
