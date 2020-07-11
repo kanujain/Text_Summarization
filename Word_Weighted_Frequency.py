9b10503dff5e3bb5e951d28083f9f503f63be847
kanujain <46599236+kanujain@users.noreply.github.com>
2020-07-12 01:05:46 +0530

    Add files via upload

---
+++ Word_Weighted_Frequency.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import Word_Frequency
+
+def calc_weighted_freq():
+    word_frequencies = Word_Frequency.calc_freq_word()
+    maximum_freq_word = max(word_frequencies.values())
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word]/maximum_freq_word)
+    return word_frequencies
+
+
+# In[ ]:
+
+
+
+
